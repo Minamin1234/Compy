6e8d46c20f579036845d7dabc58a1b405af79bff
Minamin <minamin37345@gmail.com>
2022-01-15 16:15:15 +0900

    コマンドクラスの作成，実装．

--- Compy/Compy.py
+++ Compy/Compy.py
@@ -33,3 +33,74 @@ class MStd(MModule):
             print(args[2])
         elif args[1] == self.Commands[1]:
             self.ShowHelp()
+        return
+
+class MCommand(object):
+    Modules:List[MModule] = []
+    DefaultCommands:List[str] = ["help","quit"]
+    ModuleSprt:str = "."
+    SprtInArgs:str = ","
+
+    def __init__(self) -> void:
+        newmodule = MStd()
+        self.IncludeNewModule(newmodule)
+        return
+
+    def Run(self) -> void:
+        while True:
+            cmd:str = input()
+            args:List[str] = self.EncodeArgs(cmd)
+            self.ExecuteCommand(args)
+            if args[0] == "quit":
+                break
+        return
+
+
+    def EncodeArgs(self,words:str) -> List[str]:
+        args:List[str] = [""]
+        level:int = 0
+        for w in words:
+            if w == self.ModuleSprt:
+                level = level + 1
+                args.append("")
+                continue
+            elif w == self.SprtInArgs:
+                level = level + 1
+                args.append("")
+                continue
+            elif w == "(":
+                level = level + 1
+                args.append("")
+                continue
+            elif w == ")":
+                level = level + 1
+                args.append("")
+                continue
+            args[level] = args[level] + w
+        return args
+            
+    def IncludeNewModule(self,module:MModule) -> void:
+        self.Modules.append(module)
+        return
+
+    def ShowAllModuleCommandInfo(self) -> void:
+        for module in self.Modules:
+            module.ShowHelp()
+        return
+
+    def ShowAllDefaultCommands(self) -> void:
+        print("----------DefaultCommands----------")
+        for cmd in self.DefaultCommands:
+            print(cmd)
+        return
+
+    def ExecuteCommand(self,args:List[str]) -> void:
+        print("")
+        if args[0] == self.DefaultCommands[0]:
+            self.ShowAllDefaultCommands()
+            return
+
+        for module in self.Modules:
+            if args[0] == module.ModuleName:
+                module.ExecuteCommand(args)
+        return
